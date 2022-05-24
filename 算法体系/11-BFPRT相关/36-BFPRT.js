0aba61e9c3c86c9efd059d965e5a2c1087457338
AzrDream <1499325862@qq.com>
2022-05-24 19:30:20 +0800

    BFPRT算法相关

--- 算法体系/11-BFPRT相关/36-BFPRT.js
+++ 算法体系/11-BFPRT相关/36-BFPRT.js
@@ -1,69 +1,78 @@
-function bfprt(arr, begin, end, i) {
-    if(begin === end) {
-        return arr[begin];
+function minKth3(array, k) {
+    let arr = array;
+    return bfprt(arr, 0, arr.length - 1, k - 1)
+}
+// arr[L..R]  如果排序的话，位于index位置的数，是什么，返回
+function bfprt(arr, L, R, index) {
+    if(L === R) {
+        return arr[L];
     }
-    // 求中位数
-    let pivot = medianofMedions(arr, begin, end);
-    let pivotRange = partition(arr, begin, end, pivot);
-    if(i >= pivotRange[0] && i <= pivotRange[1]) {
-        return arr[i];
-    }else if(i < pivotRange[0]) {
-        return bfprt(arr, begin, pivotRange[0] - 1, i);
+    // L...R  每五个数一组
+    // 每一个小组内部排好序
+    // 小组的中位数组成新数组
+    // 这个新数组的中位数返回
+    let pivot = medianofMedions(arr, L, R);
+    let range = partition(arr, L, R, pivot);
+    if(index >= range[0] && index <= range[1]) {
+        return arr[index];
+    }else if(index < range[0]) {
+        return bfprt(arr, L, range[0] - 1, index);
     }else {
-        return bfprt(arr, pivotRange[1] + 1, end, i);
+        return bfprt(arr, range[1] + 1, R, index);
     }
 }
-function medianofMedions(arr, begin, end) {
-    let num = end - begin + 1;
-    let offset = num % 5 === 0 ? 0:1;
+// arr[L...R]  五个数一组
+// 每个小组内部排序
+// 每个小组中位数领出来，组成marr
+// marr中的中位数，返回
+function medianofMedions(arr, L, R) {
+    let size = R - L + 1;
+    let offset = size % 5 === 0 ? 0:1;
     let mArr = [];
-    mArr.length = num / 5 + offset;
-    for(let i = 0; i < mArr.length; i++) {
-        let beginI = begin + i * 5;
-        let endI = beginI + 4;
-        mArr[i] = getMedian(arr, beginI, Math.min(end, endI));
+    mArr.length = size / 5 + offset;
+    for(let team = 0; team < mArr.length; team++) {
+        let teamFirst = L + team * 5;
+        let teamEnd = teamFirst + 4;
+        // L ... L + 4
+        // L +5 ... L +9
+        // L +10....L+14
+        mArr[team] = getMedian(arr, teamFirst, Math.min(R, teamEnd));
     }
+    // marr中，找到中位数
+    // marr(0, marr.len - 1,  mArr.length / 2 )
     return bfprt(mArr, 0, mArr.length - 1, mArr.length / 2);
 }
-function partition(arr, begin, end, pivotValue) {
-    let small = begin - 1;
-    let cur = begin;
-    let big = end + 1;
-    while(cur !== big) {
-        if(arr[cur] < pivotValue) {
-            swap(arr, ++small, cur++);
-        } else if(arr[cur] > pivotValue) {
-            swap(arr, cur, --big);
+// 获取中位数
+function getMedian(arr, L, R) {
+    insertionSort(arr, L, R);
+    return arr[(L + R) / 2];
+}
+function insertionSort(arr, L, R) {
+    for(let i = L + 1; i <= R; i++) {
+        for(let j = i - 1; j >= L && arr[j] > arr[j + 1]; j--) {
+            swap(arr, j, j + 1);
+        }
+    }
+}
+function partition(arr, L, R, pivot) {
+    let less = L - 1;
+    let more = R + 1;
+    let cur = L;
+    while(cur < more) {
+        if(arr[cur] < pivot) {
+            swap(arr, ++less, cur++);
+        } else if(arr[cur] > pivot) {
+            swap(arr, cur, --more);
         } else {
             cur++;
         }
     }
-    let range = [];
-    range[0] = small + 1;
-    range[1] = big - 1;
-    return range;
-}
-function getMedian(arr, begin, end) {
-    insertionSort(arr, begin, end);
-    let sum = end + begin;
-    let mid = (sum / 2) + (sum % 2);
-    return arr[mid];
+    return [less + 1, more - 1];
 }
 function swap(arr, i , j) {
     let temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
 }
-function insertionSort(arr, begin, end) {
-    for(let i = begin + 1; i !== end + 1; i++) {
-        for(let j = i; j !== begin; j--) {
-            if(arr[j - 1] > arr[j]) {
-                swap(arr, j - 1, j);
-            }else {
-                break;
-            }
-        }
-    }
-}
 let arr = [5,4,9,8,6,2,1,3];
-console.log(bfprt(arr, 0, arr.length - 1, 5));
+console.log(minKth3(arr,  5));
